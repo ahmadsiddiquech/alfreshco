2fc9cac57aa2b156dd3d6e473511d3b65e2c70aa
ahmadsiddiquech <muhammad.ahmad@xpertspot.com>
2020-04-09 22:02:11 +0500

    home static done

---
+++ footer.php
@@ -0,0 +1,103 @@
+<div id="back-to-top">
+            <i class="fa fa-long-arrow-up"></i>
+        </div>
+        <footer id="footer" class="footer-v3 align-left">
+              <div class="container container-ver2">
+                <div class="footer-inner">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6">
+                            <h3 class="title-footer">About Us</h3>
+                            <p>With more than 15 years of experience we can proudly say that we are one of the best in business, a trusted supplier for more than 1000 companies...</p>
+                            <a class="link-footer" href="#" title="footer">Read more <i class="fa fa-long-arrow-right"></i></a>
+                        </div>
+                        <div class="col-md-2 col-sm-6">
+                            <h3 class="title-footer">Infomation</h3>
+                            <ul class="list-footer">
+                                <li><a href="#" title="title">Delivery</a></li>
+                                <li><a href="#" title="title">Legal Notice</a></li>
+                                <li><a href="#" title="title">Terms & Conditions</a></li>
+                                <li><a href="#" title="title">About Us</a></li>
+                                <li><a href="#" title="title">Secure Payment</a></li>
+                                <li><a href="#" title="title">Our Stores</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <h3 class="title-footer">Get It Touch</h3>
+                            <div class="social space-30">
+                                <a href="#" title="t"><i class="fa fa-twitter"></i></a>
+                                <a href="#" title="f"><i class="fa fa-facebook"></i></a>
+                                <a href="#" title="y"><i class="fa fa-youtube-play"></i></a>
+                                <a href="#" title="g"><i class="fa fa-google"></i></a>
+                            </div>
+                            <h3 class="title-footer">Payment Accept</h3>
+                            <a href="#" title="paypal"><img src="assets/images/paypal-footer.png" alt="images"></a>
+                        </div>
+                        <div class="col-md-4 col-sm-6">
+                            <h3 class="title-footer">Get Newsletter</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
+                            <div class="subscribe">
+                                <form action="#" method="get" accept-charset="utf-8">
+                                    <input type="text" placeholder="Enter Your Email Address" class="input-text required-entry validate-email" title="Sign up for our newsletter" id="newsletter" name="email"> 
+                                    <button class="button button1 hover-white" title="Subscribe" type="submit">Subscribe<i class="fa fa-long-arrow-right"></i></button>
+                                </form>
+                            </div>
+                            <!-- End subscribe -->
+                        </div>
+                    </div>
+                    <!-- End row -->
+                  </div>
+                  <!-- End footer-inner -->
+              </div>
+              <!-- End container -->
+              <div class="footer-bottom box">
+                <div class="container container-ver2">
+                    <div class="box bottom">
+                        <p class="float-left">Copyright &copy;2020 XpertSpot - All Rights Reserved.</p>
+                        <div class="float-right">
+                            <ul class="menu-bottom-footer">
+                                <li><a href="#" title="Contact Us">Contact Us</a></li>
+                                <li><a href="#" title="Term of Use">Term of Use</a></li>
+                                <li><a href="#" title="Privacy Policy">Privacy Policy</a></li>
+                                <li><a href="#" title="Site Map">Site Map</a></li>
+                            </ul>
+                            <div class="language">
+                                <span>Language:</span>
+                                <div class="hover-menu">
+                                    <p>EN <i class="fa fa-angle-down"></i></p>
+                                    <div class="list-menu">
+                                        <ul>
+                                            <li><a href="#" title="EN">EN</a></li>
+                                            <li><a href="#" title="VN">VN</a></li>
+                                        </ul>
+                                    </div>    
+                                </div>
+                                <span>Price:</span>
+                                <div class="hover-menu">
+                                    <p>USD <i class="fa fa-angle-down"></i></p>
+                                    <div class="list-menu">
+                                        <ul>
+                                            <li><a href="#" title="USD">USD</a></li>
+                                            <li><a href="#" title="VND">VND</a></li>
+                                        </ul>
+                                    </div>    
+                                </div>
+                            </div>
+                            <!-- End language -->
+                        </div>
+                    </div>
+                </div>
+                <!-- End container -->
+              </div>
+        </footer>
+        </div>
+    <!-- End wrappage -->
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.themepunch.plugins.min.js"></script>
+    <script type="text/javascript" src="assets/js/engo-plugins.js"></script>
+    
+    <script type="text/javascript" src="assets/js/slick.min.js"></script>
+    <script type="text/javascript" src="assets/js/store.js"></script>
+    </body>    
+</html>
---
+++ header.php
@@ -0,0 +1,214 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/owl-slider.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/vendor/settings.css"/>
+        <link rel="shortcut icon" href="assets/images/logo.jpeg" />
+        <script type="text/javascript" src="assets/js/jquery-3.2.0.min.js"></script>
+        
+    </head>
+    <body>
+     
+    
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content popup-search">
+                <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times" aria-hidden="true"></i></button>
+                <div class="modal-body">
+                    <div class="input-group">
+                        <input type="text" class="form-control control-search" placeholder="Search and hit enter...">
+                        <button class="button_search" type="button">Search</button>
+                    </div><!-- /input-group -->
+
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>
+    <!-- End pushmenu -->
+    <div class="wrappage">
+        <!-- <div id="rtl">RTL</div> -->
+        <header id="header" class="header-v3 header-v2">
+            <div id="topbar" class="topbar-ver2">
+                <div class="container container-ver2">
+                    <div class="inner-topbar box">
+                        <div class="float-left">
+                            <p><img src="assets/images/icon-phone-header.png.png" alt="icon"> Call us <span> 070-7782-9137</span></p>
+                        </div>
+                        <div class="float-right align-right">
+                            <div class="hover-menu">
+                                <a class="acc" href="#" title="USD"><img src="assets/images/icon-user-header.png" alt="icon">MY ACCOUNT</a>
+                                <ul class="list-menu">
+                                    <li><a href="#" title="USD">LOGIN</a></li>
+                                    <li><a href="#" title="VND">REGISTER</a></li>
+                                </ul>
+                            </div>
+                            <!-- End hover-menu -->
+                        </div>
+                    </div>
+                </div>
+                <!-- End container -->
+            </div>
+            <!-- End topbar -->
+            <div class="header-top">
+                    <div class="container container-ver2">
+                    <div class="box">
+                        <p class="icon-menu-mobile"><i class="fa fa-bars" ></i></p>
+                        <div class="logo"><a href="#" title="Al Fresh Co">
+                        <img src="assets/images/logo.jpeg" alt="Al Fresh Co" height="50" width="50">
+                        </a></div>
+                        <div class="logo-mobile"><a href="#" title="Xanadu"><img src="assets/images/logo.jpeg" alt="Al Fresh Co" height="130" width="50"></a></div>
+
+                        <div class="box-right">
+                            <div class="cart hover-menu">
+                                    <p class="icon-cart" title="Add to cart">
+                                        <i class="icon"></i>
+                                        <span class="cart-count">2</span>
+                                    </p>
+                                    <div class="cart-list list-menu">
+                                        <ul class="list">
+                                            <li>
+                                                <a href="#" title="" class="cart-product-image"><img src="assets/images/products/1.jpg" alt="Product"></a>
+                                                <div class="text">
+                                                    <p class="product-name">Union Bed</p>
+                                                    <p class="product-price"><span class="price-old">$700.00</span><span class="price">$350.00</span></p>
+                                                    <p class="qty">QTY:01</p>
+                                                </div>
+                                                <a class="close" href="#" title="close"><i class="fa fa-times-circle"></i></a>
+                                            </li> 
+                                            <li>
+                                                <a href="#" title="" class="cart-product-image"><img src="assets/images/products/1.jpg" alt="Product"></a>
+                                                <div class="text">
+                                                    <p class="product-name">Union Bed</p>
+                                                    <p class="product-price"><span class="price-old">$700.00</span><span class="price">$350.00</span></p>
+                                                    <p class="qty">QTY:01</p>
+                                                </div>
+                                                <a class="close" href="#" title="close"><i class="fa fa-times-circle"></i></a>
+                                            </li>                                   
+                                        </ul>
+                                        <p class="total"><span class="left">Total:</span> <span class="right">$1121.98</span></p>
+                                        <div class="bottom">
+                                            <a class="link-v1" href="#" title="viewcart">View Cart</a>
+                                            <a class="link-v1 rt" href="#" title="checkout">Check out</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <div class="search dropdown" data-toggle="modal" data-target=".bs-example-modal-lg">
+                                <i class="icon"></i>
+                            </div>
+                        </div>
+                        <nav class="mega-menu">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                          <ul class="nav navbar-nav" id="navbar">
+                            <li class="level1 active dropdown"><a href="#" title="Home">Home</a>
+                                <ul class="menu-level-1 dropdown-menu">
+                                   <li class="level2"><a href="" title="Home 1">Home 1</a></li>
+                                    <li class="level2"><a href="" title="Home 2">Home 2</a></li>
+                                    <li class="level2"><a href="" title="Home 3">Home 3</a></li>
+                                    <li class="level2"><a href="" title="Home 4">Home 4</a></li>
+                                </ul>
+                            </li>
+                              <li class="level1"><a href="" title="About us">About us</a></li>
+                            <li class="level1 dropdown">
+                                <a href="#" title="men">Store</a>
+                                <div class="sub-menu sub-menu-v2 dropdown-menu">
+                                <div class="top-sub-menu">
+                                    <img src="assets/images/top-submenu1.jpg" alt="images">
+                                </div>
+                                  <ul class="menu-level-1">
+                                    <li class="level2"><a href="#">Living Room</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="#" title="Seatings">Seatings</a></li>
+                                            <li class="level3"><a href="#" title="Tables">Tables</a></li>
+                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
+                                            <li class="level3"><a href="#" title="Series">Series</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2"><a href="#">BATH Room</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="#" title="Lighting">Lighting</a></li>
+                                            <li class="level3"><a href="#" title="Storage">Storage</a></li>
+                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
+                                            <li class="level3"><a href="#" title="Beds">Beds</a></li>
+                                        </ul>
+                                    </li>
+                                     <li class="level2"><a href="#">KITCHEN Room</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="#" title="Cleaning Tools">Cleaning Tools</a></li>
+                                            <li class="level3"><a href="#" title="Waste Bins">Waste Bins</a></li>
+                                            <li class="level3"><a href="#" title="Pots & Pans">Pots & Pans</a></li>
+                                            <li class="level3"><a href="#" title="Beds">Beds</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2"><a href="#">Living Room</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="#" title="Seatings">Seatings</a></li>
+                                            <li class="level3"><a href="#" title="Tables">Tables</a></li>
+                                            <li class="level3"><a href="#" title="Accessories">Accessories</a></li>
+                                            <li class="level3"><a href="#" title="Series">Series</a></li>
+                                        </ul>
+                                    </li>
+                                  </ul>
+                                  <!-- End RightSubMenu -->
+                              </div>
+                              <!-- End Dropdow Menu -->
+                            </li>
+                            <li class="level1 dropdown"><a href="#" title="Page">Page</a>
+                                <div class="sub-menu dropdown-menu">
+                                  <ul class="menu-level-1">
+                                    <li class="level2"><a href="#">Shop Page</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="" title="Order">Order</a></li>
+                                            <li class="level3"><a href="" title="Shopping Cart">Shopping Cart</a></li>
+                                            <li class="level3"><a href="" title="Check Out">Check Out</a></li>
+                                            <li class="level3"><a href="" title="Wish List">Wish List</a></li>
+                                            <li class="level3"><a href="" title="Check Out">Check Out</a></li>
+                                            <li class="level3"><a href="" title="Shop Grid Full">Shop Full</a></li>
+                                            <li class="level3"><a href="" title="Shop Left Sidebar">Shop Left Sidebar </a></li>
+                                            <li class="level3"><a href="" title="Shop Right Sidebar">Shop Right Sidebar</a></li>
+
+                                        </ul>
+                                    </li>
+                                    <li class="level2"><a href="#">Other Page</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="" title="FAQ">FAQ</a></li>
+                                            <li class="level3"><a href="" title="Page 404">Page 404</a></li>
+                                            <li class="level3"><a href="" title="Newsletter">Newsletter</a></li>
+                                            <li class="level3"><a href="" title="Quick View">Quick View</a></li>
+                                            <li class="level3"><a href="" title="Login Registers">Login Register</a></li>
+                                            <li class="level3"><a href="" title="Tracking">Tracking</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="level2"><a href="#">Blog Page</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="" title="Blog 2 Columns">Blog 2 Columns</a></li>
+                                            <li class="level3"><a href="" title="Blog 3 Columns">Blog 3 Columns</a></li>
+                                            <li class="level3"><a href="" title="Blog 3 Columns Masonry">Blog 3 Columns Masonry</a></li>
+                                            <li class="level3"><a href="" title="Blog Details">Blog Details</a></li>
+                                            <li class="level3"><a href="" title="Blog List Width">Blog List Width</a></li>
+                                        </ul>
+                                    </li>
+                                     <li class="level2"><a href="#">Signle Page</a>
+                                        <ul class="menu-level-2">
+                                            <li class="level3"><a href="" title="Version 1">Product Detail Version 1</a></li>
+                                            <li class="level3"><a href="" title="Version 2">Product Detail Version 2</a></li>
+                                            <li class="level3"><a href="" title="Version 3">Product Detail Version 3</a></li>
+                                            <li class="level3"><a href="" title="Version 4">Product Detail Version 4</a></li>
+                                        </ul>
+                                    </li>
+                                  </ul>
+                                  <!-- End RightSubMenu -->
+                              </div>
+                              <!-- End Dropdow Menu -->
+                            </li>
+                            <li class="level1"><a href="" title="collections">Blog</a></li>
+                            <li class="level1"><a href="" title="Contact us">Contact us</a></li>
+                          </ul>
+                        </nav>
+                    </div>                
+                    </div>
+                    <!-- End container -->
+                </div>
+                <!-- End header-top -->
+        </header><!-- /header -->
